8c2754093e091f2250bb5bc734d1fbcefb3f3c3b
adam@callawaywilson.com <adam@callawaywilson.com>
2012-01-04 15:38:28 -0500

    Changed working on login box

--- web/content/login.php
+++ web/content/login.php
@@ -11,7 +11,7 @@
 						<input name="email" class="large_input" size="40"/>
 						<button type="submit" class="btn large primary">Start</button>
 					</div>
-					<label class="login_subtitle" for="email">Enter your email address to setup your free account or login</label>
+					<label class="login_subtitle" for="email">Enter your email to set up your free account or log in</label>
 				</form>
 			</div>
 <?php }	?>
