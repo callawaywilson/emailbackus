8b63df4442dae6dbaa3b84ac9c9ae28420ec28c7
adam@callawaywilson.com <adam@callawaywilson.com>
2012-01-04 15:45:03 -0500

    Updated Copyright year

--- web/content/footer.php
+++ web/content/footer.php
@@ -1,3 +1,3 @@
 	<div id="footer">
-		<div>&copy <a href="http://callawaywilson.com">callawaywilson.com</a> 2011. All right reserved.</div>
+		<div>&copy <a href="http://callawaywilson.com">callawaywilson.com</a> 2012. All right reserved.</div>
 	</div>
